be7a83cbd437a72341e2acb4ae94a3792cc00ba0
ay si <aysi0@MBP-de-ayoub-001.lan>
2026-06-03 16:35:33 +0200

    test: add CompleteTaskServiceTest with success and not found scenarios

---
+++ tests/Integration/Application/Task/CompleteTaskServiceTest.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+namespace Tests\Integration\Application\Task;
+
+use App\Application\Task\CompleteTask\CompleteTaskCommand;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Tests\Integration\InMemory\InMemoryTaskRepository;
+use App\Application\Task\CompleteTask\CompleteTaskService;
+use App\Domain\Task\Task;
+use App\Domain\Task\TaskId;
+use App\Domain\Task\TaskRepositoryInterface;
+use App\Domain\Task\TaskStatus;
+use App\Domain\Task\TaskTitle;
+use App\Domain\Workspace\WorkspaceId;
+use Override;
+USE PHPUnit\Framework\Attributes\CoversClass;
+use App\Domain\Task\Exception\TaskNotFoundException;
+
+#[CoversClass(CompleteTaskService::class)]
+class CompleteTaskServiceTest extends TestCase {
+    private InMemoryTaskRepository $inMemoryTaskRepository;
+    private CompleteTaskService $completeTaskService;
+    protected function setUp(): void
+    {
+        $this->inMemoryTaskRepository = new InMemoryTaskRepository();
+        $this->completeTaskService = new CompleteTaskService($this->inMemoryTaskRepository,$this->createStub(EventDispatcherInterface::class));
+    }
+    public function testCompleteTaskSuccess() :void{
+        $taskId = TaskId::generate();
+        $worksapceId = WorkspaceId::generate();
+        $taskTitle = TaskTitle::create('tertndlskf');
+        $task = Task::create($taskId,$taskTitle,$worksapceId);
+        $this->inMemoryTaskRepository->save($task);
+        $command =  new CompleteTaskCommand($taskId->value());
+        $this->completeTaskService->complete($command);
+        $this->assertSame(TaskStatus::COMPLETED, $this->inMemoryTaskRepository->findById($taskId)->status());
+    }
+
+    public function testCompleteTaskThrowsExeptionWhenTaskNotFound() {
+        $this->expectException(TaskNotFoundException::class);
+        $taskId = TaskId::generate();
+        $worksapceId = WorkspaceId::generate();
+        $taskTitle = TaskTitle::create('tertndlskf');
+        $task = Task::create($taskId,$taskTitle,$worksapceId);
+        $command =  new CompleteTaskCommand($taskId->value());
+        $this->completeTaskService->complete($command);
+        $taskfound = $this->inMemoryTaskRepository->findById($taskId);
+    }
+}
