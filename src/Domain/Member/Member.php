fc25ce32bc5036725f057fe5864f07a54040f2ae
ay si <aysi0@MBP-de-ayoub-001.lan>
2026-06-03 18:57:20 +0200

    feat: complete Member aggregate with role change invariant and private constructor

--- src/Domain/Member/Member.php
+++ src/Domain/Member/Member.php
@@ -6,21 +6,17 @@ namespace App\Domain\Member;
 
 use App\Domain\Member\Event\MemberCreated;
 use App\Domain\Shared\AggregateRoot;
+use App\Domain\Member\Exception\InvalidMemberRoleChangeException;
 
 /**
  * Summary of Member
  */
 final class Member extends AggregateRoot
 {
-    private MemberId $id;
-    private MemberRole $role;
-    public function __construct(
-        MemberId $id,
-        MemberRole $role
+    private function __construct(
+        private readonly MemberId $id,
+        private MemberRole $role
     ) {
-        $this->id = $id;
-        $this->role = $role;
-
     }
     /**
      * Summary of create
@@ -49,5 +45,10 @@ final class Member extends AggregateRoot
     public function role(): MemberRole
     {
         return $this->role;
+    }
+
+    public function changeRole(MemberRole $memberRole) :void {
+        if($this->role === MemberRole::OWNER) throw new InvalidMemberRoleChangeException("Impossible de changer role Owner");
+        $this->role = $memberRole;
     }
 }
