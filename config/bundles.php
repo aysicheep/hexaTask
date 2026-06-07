c7cb54cfd2269e90589a939dc840b4254329a395
ay si <aysi0@Host-003.lan>
2026-06-07 17:10:21 +0200

    chore: remove test.php debug file

--- config/bundles.php
+++ config/bundles.php
@@ -1,4 +1,7 @@
-<?php 
-return[
+<?php
+
+return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
